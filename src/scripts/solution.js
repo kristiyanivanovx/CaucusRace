70b348a5c34a5ab719b0c1ae03c396197ed03e18
kristiyanivanovx <kristianiva88@gmail.com>
2021-07-04 21:35:30 +0300

    fix code formatting and convert spaces to tabs
    fix code formatting and convert spaces to tabs

--- src/scripts/solution.js
+++ src/scripts/solution.js
@@ -7,73 +7,73 @@ submitButton.addEventListener("click", caucusRace);
 clearButton.addEventListener("click", clearInputOutput);
 
 function clearInputOutput(e) {
-    e.preventDefault();
-    outputDiv.innerHTML = "";
-    input.value = "";
+	e.preventDefault();
+	outputDiv.innerHTML = "";
+	input.value = "";
 }
 
 function checkValidIndex(inputArray, currIndex) {
-    let shouldBeAdded = true;
-    let innerScore = 0;
+	let shouldBeAdded = true;
+	let innerScore = 0;
 
-    for (let j = currIndex; j < inputArray.length; j++) {
-        innerScore += inputArray[j];
+	for (let j = currIndex; j < inputArray.length; j++) {
+		innerScore += inputArray[j];
 
-        if (innerScore <= 0) {
-            shouldBeAdded = false;
-        }  
-    }
+		if (innerScore <= 0) {
+			shouldBeAdded = false;
+		}
+	}
 
-    for (let k = 0; k < currIndex; k++) {
-        innerScore += inputArray[k];
+	for (let k = 0; k < currIndex; k++) {
+		innerScore += inputArray[k];
 
-        if (innerScore <= 0) {
-            shouldBeAdded = false;
-        }  
-    }
-  
-    return shouldBeAdded;
+		if (innerScore <= 0) {
+			shouldBeAdded = false;
+		}
+	}
+
+	return shouldBeAdded;
 }
 
 function caucusRace(e) {
-    e.preventDefault();
-    
-    // if the input is an empty string
-    if (input.value === "") {
-        return;
-    }
+	e.preventDefault();
 
-    // extract all numbers with regex and make them integers
-    let inputArray = input.value.match(/-?\d+/gm).map(element => Number(element));
+	// if the input is an empty string
+	if (input.value === "") {
+		return;
+	}
 
-    let output = [];
-    for (let currIndex = 0; currIndex < inputArray.length; currIndex++) {
-        let shouldBeAdded = checkValidIndex(inputArray, currIndex);
-        
-        if (shouldBeAdded) {
-            output.push(currIndex);
-        }
-    }
+	// extract all numbers with regex and make them integers
+	let inputArray = input.value.match(/-?\d+/gm).map(element => Number(element));
 
-    output = output.sort((a, b) => a - b);
+	let output = [];
+	for (let currIndex = 0; currIndex < inputArray.length; currIndex++) {
+		let shouldBeAdded = checkValidIndex(inputArray, currIndex);
 
-    // prepare output heading section
-    let headingDiv = document.createElement("div");
-    headingDiv.setAttribute("class", "col-md-4 offset-md-4");
-    headingDiv.innerHTML = "<h3>Output</h3><hr>";
-    outputDiv.appendChild(headingDiv);
+		if (shouldBeAdded) {
+			output.push(currIndex);
+		}
+	}
 
-    // set values for output and append
-    let answerDiv = document.createElement("div");
-    answerDiv.setAttribute("class", "col-md-4 offset-md-4 alert alert-success ml-1 mt-1 mb-1 mr-1");
-    
-    // styled as array version
-    let outputStyled = "[" + output.join(', ') + "]";
+	output = output.sort((a, b) => a - b);
 
-    // real array version
-    // let outputStyled = output;
+	// prepare output heading section
+	let headingDiv = document.createElement("div");
+	headingDiv.setAttribute("class", "col-md-4 offset-md-4");
+	headingDiv.innerHTML = "<h3>Output</h3><hr>";
+	outputDiv.appendChild(headingDiv);
 
-    answerDiv.innerHTML = outputStyled;
-    outputDiv.appendChild(answerDiv);
-    console.log(output);
+	// set values for output and append
+	let answerDiv = document.createElement("div");
+	answerDiv.setAttribute("class", "col-md-4 offset-md-4 alert alert-success ml-1 mt-1 mb-1 mr-1");
+
+	// styled as array version
+	let outputStyled = "[" + output.join(', ') + "]";
+
+	// real array version
+	// let outputStyled = output;
+
+	answerDiv.innerHTML = outputStyled;
+	outputDiv.appendChild(answerDiv);
+	console.log(output);
 }
